3b827ee80e1fead96ccde022495b122eebccac98
Josh Чернов <961877+joshchernoff@users.noreply.github.com>
2024-07-02 16:21:12 -0700

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <!-- src/App.vue -->
 <template>
   <TodoApp />
+  <Footer />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TodoApp from './components/TodoApp.vue';
+import Footer from './components/Footer.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     TodoApp,
+    Footer,
   },
 });
 </script>
