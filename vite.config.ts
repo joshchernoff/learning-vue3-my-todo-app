496b5017debb03335033ae6b94186bab651edefc
Josh Чернов <961877+joshchernoff@users.noreply.github.com>
2024-07-02 15:57:03 -0700

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/learning-vue3-my-todo-app/', // replace with your repository name
   plugins: [vue()],
 });
